39b7ade8ca36d557dbe2e8eb1896d113d369a2cf
Bill Cathey <billcathey@hotmail.com>
2021-10-05 17:59:16 -0400

    Refactor to import data

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Card = () => {
-  return <h2>Movie #1</h2>;
+const Card = ({ movie }) => {
+  return (
+    <div>
+      <h2>{`#${movie.ranking} ${movie.title}`}</h2>
+      <img src={movie.img.src} alt={movie.img.alt} />
+      <p>{`Director: ${movie.director}`}</p>
+      <p>{`Year: ${movie.year}`}</p>
+    </div>
+  );
 };
 
 export default Card;
--- src/containers/List.js
+++ src/containers/List.js
@@ -1,9 +1,29 @@
 import React, { Component } from "react";
-import Card from '../components/Card/Card'
+import Card from "../components/Card/Card";
+const moviesJSON = require("../../src/assets/data.json");
 
 class List extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      loading: true,
+    };
+  }
+  async componentDidMount() {
+    if (moviesJSON) {
+      this.setState({
+        data: moviesJSON,
+        loading: false,
+      });
+    }
+  }
   render() {
-    return <Card />;
+    const { data, loading } = this.state;
+    if (loading) {
+      return <div>Loading ...</div>;
+    }
+    return data.map((movie) => <Card key={movie.id} movie={movie} />);
   }
 }
 
